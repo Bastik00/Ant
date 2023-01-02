8660ac5f6cbac6b6dc4d0a3c4adb0549ff23cca9
Sebastian Krause <sebastian.krause@endress.com>
2023-01-02 18:59:23 +0100

    add close button hover

--- RoboView/Gui/InternalWindow/WindowCloser.py
+++ RoboView/Gui/InternalWindow/WindowCloser.py
@@ -7,13 +7,23 @@ class WindowCloser():
         self._root = root
         self._canvas = Canvas(root)
         self._master = master
-        self._image = PhotoImage(file="Ant/close.png")
         
+        self._image = PhotoImage(file="Ant/Icons/close.png")
         self._canvas.create_image(0,0, image=self._image, anchor='nw')
         self._canvas.bind("<ButtonRelease-1>", self.mouse_released)
+        self._canvas.bind("<Enter>", self.mouse_hover)
+        self._canvas.bind("<Leave>", self.mouse_leave)
+        
 
- 
     def mouse_released(self,event):
         self._master.close()
+        
+    def mouse_hover(self,event):
+        self._image = PhotoImage(file="Ant/Icons/close_hover.png")
+        self._canvas.create_image(0,0, image=self._image, anchor='nw')
+        
+    def mouse_leave(self,event):
+        self._image = PhotoImage(file="Ant/Icons/close.png")
+        self._canvas.create_image(0,0, image=self._image, anchor='nw')
 
 
